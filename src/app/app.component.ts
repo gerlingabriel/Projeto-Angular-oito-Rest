fae983bfd2566728f5e077eb957431ed17e2f69e
GabrielGerlin <gerlin.gabriel@gmail.com>
2020-12-23 17:37:44 -0300

    AULA 12 - Criando a Barra de Menu
    Criando a Barra de Menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
-import { Component } from '@angular/core';
-import { LoginServiceService } from './service/login-service.service';
-
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
-export class AppComponent {
-  title = 'Projeto-Angular-oito-Rest';
+export class AppComponent implements OnInit {
+  title = "Projeto-Angular-oito-Rest";
 
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    if (localStorage.getItem("token") == null) {
+      this.router.navigate(["login"]);
+    }
+  }
+  public sair(){
+    localStorage.clear();
+    this.router.navigate(["login"]);
+  }
 }
